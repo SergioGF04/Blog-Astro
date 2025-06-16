e28bd617de08ee31fa59e6b33f9f088e21ba284b
SergioGF04 <sergiogalvanf04@gmail.com>
2025-06-17 00:36:18 +0200

    Blog Astro terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon({
+    include: {
+      lucide: ["*"]
+    }
+  }), sitemap()],
+  site: "http://localhost:4321/"
+});
---
+++ src/content/blog/third-post.md
@@ -0,0 +1,10 @@
+---
+title: "My Third Post"
+pubDate: "Jun 18 2025"
+heroImage: "/assets/blog-placeholder-2.jpg"
+tags: ["AI", "JavaScript", "Frameworks"]
+---
+
+# Titulo principal
+
+lorem lorem lorem lorem lorem lorem lorem lorem
